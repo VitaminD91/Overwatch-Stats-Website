607c8747d8b5521e28062904ff2b5f9f4f244f5e
Darren <40284701@live.napier.ac.uk>
2018-11-18 17:52:19 +0000

    added get_stats method and background

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 from flask import Flask, render_template
 import json
+import requests
 
 app = Flask(__name__)
+headers = { 'User-Agent': 'Python OW app 1.0' }
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+def get_stats(battletag):
+    response = requests.get(f"https://owapi.net/api/v3/u/{battletag}/stats", headers=headers)
+    if response.status_code != 200:
+        return None
+    return response
+    
+
+    
