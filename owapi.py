1ccd2314270f85d8588971b17b600f25b0277724
Darren <40284701@live.napier.ac.uk>
2018-12-03 01:01:44 +0000

    Update owapi.py

--- owapi.py
+++ owapi.py
@@ -9,7 +9,7 @@ headers = { 'User-Agent': 'Python OW app 1.0' }
 
 def get_profile(battletag):
     encoded_battletag = battletag.replace("-", "%23")
-    response = requests.get("https://playoverwatch.com/en-gb/search/account-by-name/" + {encoded_battletag})
+    response = requests.get("https://playoverwatch.com/en-gb/search/account-by-name/" + str({encoded_battletag}))
     if response.status_code != 200:
         return None
     profile = json.loads(response.content)
@@ -28,21 +28,21 @@ def search_players(query):
     return search_result
 
 def get_stats(battletag, region):
-    response = requests.get("https://owapi.net/api/v3/u/" + {battletag} + "/stats", headers=headers)
+    response = requests.get("https://owapi.net/api/v3/u/" + str({battletag}) + "/stats", headers=headers)
     if response.status_code != 200:
         return None
     stats = json.loads(response.content)
     return stats[region]['stats']
 
 def get_heroes(battletag, region):
-    response = requests.get("https://owapi.net/api/v3/u/" + {battletag} + "/heroes", headers=headers)
+    response = requests.get("https://owapi.net/api/v3/u/" + str({battletag}) + "/heroes", headers=headers)
     if response.status_code != 200:
         return None
     heroes = json.loads(response.content)
     return heroes[region]['heroes']
 
 def get_blob(battletag, region):
-    url = "https://owapi.net/api/v3/u/" + {battletag} + "/blob"
+    url = "https://owapi.net/api/v3/u/" + str({battletag}) + "/blob"
     response = requests.get(url, headers=headers)
     #API returns error 429 when being ratelimited - wait for specified time and try again
     if response.status_code == 429:
